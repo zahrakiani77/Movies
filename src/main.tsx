234c2afe8458a1127a19838b584c74a4dc28d30d
ZahraKiani <zahra.kiani7776@gmail.com>
2025-09-04 10:09:50 +0330

    init project

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import { Provider } from './components/ui/provider.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const client = new QueryClient();
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <Provider>
+      <QueryClientProvider client={client}>
+      <App />
+      </QueryClientProvider>
+    </Provider>
+  </StrictMode>
+);
