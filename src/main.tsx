2ecf3418439357a617b17cba71b6f98c0824b674
hadis <hadissdehbashii@gmail.com>
2025-09-06 10:17:02 +0330

    Add ESLint and Prettier configuration files; update dependencies and scripts

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { Provider } from './components/ui/provider.tsx'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import { Provider } from './components/ui/provider.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const client = new QueryClient();
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider>
       <QueryClientProvider client={client}>
-      <App />
+        <App />
       </QueryClientProvider>
     </Provider>
   </StrictMode>
