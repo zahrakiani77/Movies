5f80e43a44453aa84cefc34149b26e9d2e66ccd4
hadis <hadissdehbashii@gmail.com>
2025-09-18 18:51:31 +0330

    refactor: :recycle: improve error handling and state initialization in useTrending hook

--- src/hooks/useTrending.tsx
+++ src/hooks/useTrending.tsx
@@ -2,20 +2,23 @@ import type { TrendingModel } from "../types/trending.model";
 import { axiosInstance } from "../utils/lib"
 import { useEffect, useState } from "react";
 
-export const useTrending=()=>{
-    const [data, setData] = useState<TrendingModel | null>();
-     const [isLoading, setisLoading] = useState<boolean>(false);
-      const [error, setError] = useState<string>('');
-    useEffect(()=>{
+export const useTrending = () => {
+    const [data, setData] = useState<TrendingModel | null>(null);
+    const [isLoading, setisLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string>('');
+    useEffect(() => {
         setisLoading(true);
         axiosInstance
           .get(`/trending/all/day?language=en-US`)
           .then((res) => {
             setData(res.data);
-             setisLoading(false);
-          }).catch(err =>setError(err))
-            
-    },[])
-    return {data, isLoading, error};
+            setisLoading(false);
+          })
+          .catch((err) => {
+            setError(String(err));
+            setisLoading(false);
+          });
+    }, []);
+    return { data, isLoading, error };
 }
 
